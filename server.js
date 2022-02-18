5063672792791350b52924fe10fd5d043c770de7
Beryl-18 <beryla.s@hotmail.com>
2022-02-18 13:19:15 +0400

    adding more request, prep files for heroku dep.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+
+//using cors
+app.use(cors());
 
 //connecting to mongodbclient
 let db; 
@@ -9,15 +13,27 @@ MongoClient.connect('mongodb+srv://beryl3145:berylabeysam@cluster0.ocqvc.mongodb
     db = client.db('3145');
 })
 
-app.use(express.json());
-
-
 //handling the collection request
-app.param('collectionName', function(req, res, next){
+app.param('collectionName', function(req, res, next, collectionName){
     req.collection = db.collection(collectionName);
     return next();
 
 });
+
+//express json parser
+app.use(express.json());
+
+//Middleware Logger
+app.use('/', (req,res,next) => {
+    console.log("Request url : "+req.url);
+    console.log("Request Method : "+req.method);
+    next();
+});
+
+//default request
+app.get('/', (req,res) =>{
+    res.send("please select a /collection");
+})
 
 //retrieving and sending collection requests
 app.get('/collection/:collectionName', (req, res, next) => {
@@ -27,17 +43,44 @@ app.get('/collection/:collectionName', (req, res, next) => {
     })
 });
 
+//retrieving a single item from collectionName
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+            if (e) return next(e)
+            res.send(result);
+        }) 
+});
+
 //creating a default get request handler
-app.get('/', function (request,response){
+app.get('/collection', function (request,response){
     response.send("please select a route - eg. /collection/collectionName");
 })
 
 //handling post insert requests
 app.post('/collection/:collectionName', (req, res, next) =>{
-    req.collectionName.insert(req.body, (e,results) =>{
+    req.collection.insertOne(req.body, (e,results) =>{
         if(e) next (e)
         res.send(results.ops)
     })
 })
 
-app.listen(3000);
+//handling update requests for spaces
+app.put('/collection/:collectionName/:id/', (req, res, next) =>{
+    // var updatedSpace;
+
+    req.collection.updateOne(
+        { _id: new ObjectID(req.params.id)},
+        { $set : req.body},
+        {$safe : true, multi : false},
+    ),(e,result) =>{
+        if (e) return next (e)
+        res.send((result.result.n == 1)?{msg : "success"} : {msg : "error"})
+    }
+})
+
+//App Listening at port 3000
+const port = process.env.PORT || 3000;
+app.listen(port, function (){
+    console.log("Server is running at port 3000");
+});
