2044e0ec6e5433ac49c88449b1ebb9e2a60599b3
Beryl-18 <beryla.s@hotmail.com>
2022-03-01 15:07:17 +0400

    registering service worker

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+var cacheName = 'lesson-v1';
+var cacheFiles = [
+  'index.html',
+  'server.js',
+  'stylesheet.css',
+  'lesson.webmanifest',
+  'images/biology.jpg',
+  'images/chemistry.jpg',
+  'images/cricket.jpg',
+  'images/culinary.jpg',
+  'images/culinary.jps.jpeg',
+  'images/finance.jpg',
+  'images/french.jpg',
+  'images/icon.png',
+  'images/math.jpg',
+  'images/music.jpg',
+  'images/pottery.jpg',
+  'images/soccer.png',
+  'images/icon-store-512.png'
+];
