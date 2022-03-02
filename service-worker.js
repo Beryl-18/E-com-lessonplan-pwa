8456a34b64afd172d18e45189780735db771b372
Beryl-18 <beryla.s@hotmail.com>
2022-03-02 17:04:10 +0400

    adding file to cache

--- service-worker.js
+++ service-worker.js
@@ -17,32 +17,19 @@ var cacheFiles = [
   'images/math.jpg',
   'images/music.jpg',
   'images/pottery.jpg',
-  'images/soccer.png'
+  'images/soccer.png',
+  'service-worker.js'
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (e) =>{
   console.log('[Service Worker] Install');
   e.waitUntil(
-      cache.open(cacheName).then((cache) =>{
-          console.log('[Service Worker] caching all files');
+      caches.open(cacheName).then((cache) => {
+          console.log("[Service Worker] Caching all the files");
           return cache.addAll(cacheFiles);
       })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  console.log("[Service Worker] fetched resource"+e.request.url);
-});
-
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    //checking if files exist
-    caches.match(e.request).then(function (r) {
-      console.log("[Service Worker] fetching resource :"+e.request.url );
-      return r;
-    }
-  ));
-});
+  )
+})
 
 self.addEventListener('fetch', function (e) {
   e.respondWith(
