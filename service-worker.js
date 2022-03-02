83cf733dff9cfc7bcabec52955b63aab7b31f448
Beryl-18 <beryla.s@hotmail.com>
2022-03-02 16:37:54 +0400

    finishing the sw.js

--- service-worker.js
+++ service-worker.js
@@ -28,6 +28,31 @@ self.addEventListener('install', (e) => {
           return cache.addAll(cacheFiles);
       })
   );
-  
 });
 
+self.addEventListener('fetch', (e) => {
+  console.log("[Service Worker] fetched resource"+e.request.url);
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    //checking if files exist
+    caches.match(e.request).then(function (r) {
+      console.log("[Service Worker] fetching resource :"+e.request.url );
+      return r;
+    }
+  ));
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return r || fetch (e.request).then (function (response){
+        return caches.open(cacheName).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        })
+      })
+    })
+  )
+});
