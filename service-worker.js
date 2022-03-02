332a180ba3a763d789a26b6546ab70488a844c0c
Beryl-18 <beryla.s@hotmail.com>
2022-03-02 17:09:51 +0400

    changing the fetch

--- service-worker.js
+++ service-worker.js
@@ -31,15 +31,16 @@ self.addEventListener('install', (e) =>{
   )
 })
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (r) {
-      return r || fetch (e.request).then (function (response){
-        return caches.open(cacheName).then((cache) => {
-          cache.put(e.request, response.clone());
-          return response;
+self.addEventListener( 'fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            return r || fetch (e.request).then(function (response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request,response.clone());
+                    return response;
+                })
+
+            })
         })
-      })
-    })
-  )
-});
+    )
+})
